b962e5073b0402c3564991bb8134c942537bfd30
xavimarin35 <36265010+xavimarin35@users.noreply.github.com>
2018-10-05 21:04:07 +0200

    map1 update

---
+++ INDIANA JUMPS/Game/maps/BG.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.0" name="BG" tilewidth="32" tileheight="32" tilecount="1200" columns="40">
+ <image source="../../../deserttileset/png/BG.png" width="1280" height="960"/>
+</tileset>
