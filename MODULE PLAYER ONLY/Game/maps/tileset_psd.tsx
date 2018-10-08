6b15c01c9631730881f68eeb443b00a714fcd088
xavimarin35 <36265010+xavimarin35@users.noreply.github.com>
2018-10-08 13:19:25 +0200

    deleted extra folder

--- MODULE PLAYER ONLY/Game/maps/tileset_psd.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="tileset_psd" tilewidth="32" tileheight="32" tilecount="512" columns="32">
- <image source="tileset_psd.png" width="1024" height="512"/>
-</tileset>
