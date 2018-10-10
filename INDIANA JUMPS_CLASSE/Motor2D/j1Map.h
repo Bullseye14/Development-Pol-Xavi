eb3177e89b48b30e4c3c4ccc916f9d4591e23d27
xavimarin35 <36265010+xavimarin35@users.noreply.github.com>
2018-10-10 12:17:53 +0200

    fix

--- INDIANA JUMPS_CLASSE/Motor2D/j1Map.h
+++ INDIANA JUMPS_CLASSE/Motor2D/j1Map.h
@@ -113,7 +113,7 @@ public:
 	// TODO 8: Create a method that translates x,y coordinates from map positions to world positions
 	iPoint MapToWorld(int x, int y) const;
 
-private:
+public:
 
 	bool LoadMap();
 	bool LoadTilesetDetails(pugi::xml_node& tileset_node, TileSet* set);
