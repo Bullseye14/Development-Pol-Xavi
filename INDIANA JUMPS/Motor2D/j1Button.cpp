5da8805c8178830b9e57f39a1588aee186e539f7
Bullseye14 <36154277+Bullseye14@users.noreply.github.com>
2018-12-12 17:22:02 +0100

    New main menu images

--- INDIANA JUMPS/Motor2D/j1Button.cpp
+++ INDIANA JUMPS/Motor2D/j1Button.cpp
@@ -6,12 +6,16 @@
 #include "j1Textures.h"
 #include "j1Fonts.h"
 #include "j1Window.h"
+#include "j1MainMenu.h"
+#include "j1Scene.h"
 
 j1Button::j1Button(const char* text, BUTTON_TYPE type)
 {
 	buttonRect = { 0,0,133,50 };
 	buttonOnHover = { 0,50,133,50 };
 	buttonOnClick = { 0,100,133,50 };
+
+	b_type = type;
 
 	buttonText.create(text);
 }
@@ -43,6 +47,19 @@ bool j1Button::PostUpdate()
 
 	case MOUSE_CLICK:
 		ret = App->render->Blit(App->gui->GetAtlas(), position.x, position.y, &buttonOnClick);
+		if (b_type == PLAY) 
+		{
+			App->mainmenu->GoToScene();
+		}
+		else if (b_type == CONTINUE) 
+		{
+			App->mainmenu->GoToScene();
+			App->LoadGame("save_game.xml");
+		}
+		else if (b_type == EXIT) 
+		{
+			return ret = false;
+		}
 		break;
 
 	}
@@ -67,7 +84,7 @@ bool j1Button::OnClick()
 
 	if (OnHover())
 	{
-		if (App->input->GetMouseButtonDown(SDL_BUTTON_LEFT) == KEY_REPEAT)
+		if (App->input->GetMouseButtonDown(1) == KEY_REPEAT)
 		{
 			ret = true;
 		}
