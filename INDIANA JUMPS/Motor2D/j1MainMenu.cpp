bf6942114a2b7ec57261f03f63843c56404c5de5
Bullseye14 <36154277+Bullseye14@users.noreply.github.com>
2018-12-12 19:41:31 +0100

    Animations correct

--- INDIANA JUMPS/Motor2D/j1MainMenu.cpp
+++ INDIANA JUMPS/Motor2D/j1MainMenu.cpp
@@ -7,26 +7,35 @@
 #include "j1UI_Element.h"
 #include "j1Input.h"
 #include "j1EntityManager.h"
+#include "j1Fonts.h"
 
 j1MainMenu::j1MainMenu()
 {
 	name.create("mainmenu");
 
-	idle.PushBack({ 0,0,156,245 });
-	idle.PushBack({ 162,0,156,245 });
-	idle.PushBack({ 0,245,156,245 });
-	idle.PushBack({ 162,245,156,245 });
-	idle.PushBack({ 0,490,156,245 });
-	idle.PushBack({ 162,490,156,245 });
-	idle.PushBack({ 0,735,156,245 });
-	idle.PushBack({ 162,735,156,245 });
-	idle.PushBack({ 0,978,156,245 });
-	idle.PushBack({ 162,978,156,245 });
+	idle.PushBack({ 0,0,156,244 });
+	idle.PushBack({ 162,0,155,244 });
+	idle.PushBack({ 1,245,157,244 });
+	idle.PushBack({ 163,245,155,244 });
+	idle.PushBack({ 3,490,155,244 });
+	idle.PushBack({ 164,491,156,244 });
+	idle.PushBack({ 3,735,155,244 });
+	idle.PushBack({ 162,734,156,244 });
+	idle.PushBack({ 1,978,157,244 });
+	idle.PushBack({ 161,978,156,244 });
 	idle.speed = 0.2f;
 
-	running.PushBack({ 150,150,156,245 });
-	running.PushBack({ 150,150,156,245 });
-	running.PushBack({ 150,150,156,245 });
+	running.PushBack({ 326,10,202,239 });
+	running.PushBack({ 530,8,203,247 });
+	running.PushBack({ 326,260,195,245 });
+	running.PushBack({ 530,262,202,232 });
+	running.PushBack({ 326,520,202,236 });
+	running.PushBack({ 530,520,204,236 });
+	running.PushBack({ 326,772,195,246 });
+	running.PushBack({ 530,768,198,238 });
+	running.PushBack({ 326,1025,195,225 });
+	running.PushBack({ 530,1030,204,233 });
+	running.speed = 0.2f;
 
 }
 
@@ -58,6 +67,11 @@ bool j1MainMenu::Start()
 	App->gui->element_list.add(App->gui->SpawnButton(800, 235, CREDITS, "CREDITS"));
 	App->gui->element_list.add(App->gui->SpawnButton(800, 305, EXIT, "EXIT"));
 
+
+	//Loading Fonts
+	App->font->font_title = App->font->Load("fonts/Adventure.ttf", 12);
+	App->font->font_buttons = App->font->Load("fonts/West.ttf", 8);
+
 	return true;
 }
 
@@ -65,23 +79,13 @@ bool j1MainMenu::Update(float dt)
 {
 	App->render->Blit(menuBackgroundTex, 0, 0, &BG_Rect);
 
-	if (App->gui->mouse_hovering) 
-	{
-		current_animation = &running;
-	}
-	
-	if(!App->gui->mouse_hovering)
-	{
-		current_animation = &idle;
-	}
-
-	App->render->Blit(IndianaJumps, 200, 148, &current_animation->GetCurrentFrame());
-
 	return true;
 }
 
 bool j1MainMenu::PostUpdate()
 {
+	App->render->Blit(IndianaJumps, 200, 148, &current_animation->GetCurrentFrame());
+
 	return true;
 }
 
